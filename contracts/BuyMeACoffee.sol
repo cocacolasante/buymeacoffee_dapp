029cde9f64ee8456aeeeef076c146db52990ff23
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-09-09 21:47:04 -0400

    buy coffee and withdraw functions complete tested

--- contracts/BuyMeACoffee.sol
+++ contracts/BuyMeACoffee.sol
@@ -7,26 +7,76 @@ contract BuyMeACoffee{
     // storage variables
     address payable public owner;
     uint256 public coffeePrice = 2 ether;
+    uint public totalCoffees;
+    uint public balanceOfContract;
     
     //mapping of address to tip amount
     mapping(address=>uint) public coffeeBuyers;
+    mapping(address => string) public coffeeMessages;
 
     // event of coffee bought
 
     event CoffeeBought(
-        uint _timestampe,
+        uint _timestamp,
         address _coffeeBuyer,
-        uint _coffeesBought
+        uint _coffeesBought,
+        string _message
     );
 
-    constructor(){
-        owner = payable(msg.sender);
-        console.log("I am constructed");
+    struct CoffeeBuy{
+        uint _timestamp;
+        address _coffeeBuyer;
+        uint _coffeesBought;
+        string _message;
     }
 
-    function buyCoffee(uint _amount, address _sender) external {
-        require(_amount >= coffeePrice, "Coffee costs more than that!");
+    CoffeeBuy[] public coffeeBuys;
+
+    constructor() payable {
+        owner = payable(msg.sender);
+    }
+
+    modifier onlyOwner{
+        require(msg.sender == owner, "Only owner can call function");
+        _;
+    }
+
+    receive() external payable {}
+    fallback() external payable {}
+
+    function buyCoffee(string memory message) external payable {
+        require(msg.value >= coffeePrice, "Coffee costs more than that!");
+        uint coffee = (msg.value / coffeePrice);
         
+        
+
+        coffeeBuys.push(CoffeeBuy(
+            block.timestamp,
+            msg.sender,
+            coffee,
+            message
+        ));
+
+        coffeeMessages[msg.sender] = message;
+        totalCoffees += coffee;
+        balanceOfContract += msg.value;
+
+        emit CoffeeBought(block.timestamp, msg.sender, coffee, message);
+
+
+
+    }
+
+    function getAllCoffeeBuys() public view returns(CoffeeBuy[] memory){
+        return coffeeBuys;
+    }
+
+    function getTotalCoffee() public view returns(uint){
+        return(totalCoffees);
+    }
+
+    function withdraw() public {
+        require(owner.send(address(this).balance));
     }
 
 
--- test/buyCoffeeTest.js
+++ test/buyCoffeeTest.js
@@ -1,10 +1,13 @@
-const { expect } = require("chai");
+const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Buy Me A Coffee Smart Contract", ()=>{
 
     let CoffeeContract;
     const coffeePriceTest = BigInt(2000000000000000000)
+    const oneCoffee = BigInt(2000000000000000000)
+    const twoCoffees = BigInt(4000000000000000000)
+    const fourCoffees = BigInt(8000000000000000000)
     const coffeePurchase1 = BigInt(5000000000000000000)
 
     beforeEach(async () => {
@@ -13,15 +16,78 @@ describe("Buy Me A Coffee Smart Contract", ()=>{
         CoffeeContract = await BuyContractFactory.deploy();
         await CoffeeContract.deployed();
         
-        console.log("Contract deployed to:", CoffeeContract.address);
+        // console.log("Contract deployed to:", CoffeeContract.address);
+        
       }) 
     it("Checks the price of a coffee", async()=>{
         expect(await CoffeeContract.coffeePrice()).to.be.equal(coffeePriceTest)
     })
-    it("Checks the buy coffee function", async () => {
+    it("Checks the buy coffee function transfer funds to contract", async () => {
+        const [deployer, user1] = await ethers.getSigners()
+
+
+        let txn = await CoffeeContract.connect(user1).buyCoffee("testing1", {value: twoCoffees})
+        await txn.wait()
+
+
+        expect(await CoffeeContract.totalCoffees()).to.equal(2)
+        expect(await CoffeeContract.balanceOfContract()).to.equal(twoCoffees)
+
+    })
+    it("Checks for an event and new coffee buys added to struct", async () =>{
+
+        const [deployer, user1] = await ethers.getSigners()
+
+        let txn = await CoffeeContract.connect(user1).buyCoffee("testing1", {value: twoCoffees})
+        await txn.wait()
+
+        assert(await CoffeeContract.coffeeBuys(0))
+        
+            
+    })
+    it("Checks get all coffee buys fuction", async () => {
+        const [user1, user2, user3] = await ethers.getSigners()
+
+        let txn = await CoffeeContract.connect(user1).buyCoffee("testing1", {value: twoCoffees})
+        await txn.wait()
+
+        await CoffeeContract.connect(user1).buyCoffee("testing1", {value: twoCoffees})
+        await CoffeeContract.connect(user1).buyCoffee("testing1", {value: twoCoffees})
+        await CoffeeContract.connect(user1).buyCoffee("testing1", {value: twoCoffees})
+
+        assert(await CoffeeContract.getAllCoffeeBuys())
+    })
+    it("Check total coffees function", async () => {
         const [user1] = await ethers.getSigners()
 
-        let txn = await CoffeeContract.connect(user1).buyCoffee(coffeePurchase1)
+        let txn = await CoffeeContract.connect(user1).buyCoffee("testing1", {value: twoCoffees})
         await txn.wait()
+
+        await CoffeeContract.connect(user1).buyCoffee("testing1", {value: twoCoffees})
+        await CoffeeContract.connect(user1).buyCoffee("testing1", {value: twoCoffees})
+        await CoffeeContract.connect(user1).buyCoffee("testing1", {value: twoCoffees})
+
+        expect(await CoffeeContract.getTotalCoffee()).to.equal(8)
     })
+    it("test the withdraw function", async () => {
+        const [deployer, user1] = await ethers.getSigners()
+
+        let txn = await CoffeeContract.connect(user1).buyCoffee("testing1", {value: twoCoffees})
+        await txn.wait()
+
+        const initialBalance = BigInt(await deployer.getBalance())
+        console.log(initialBalance)
+
+        expect(await CoffeeContract.balanceOfContract()).to.equal(twoCoffees)
+        await CoffeeContract.connect(user1).buyCoffee("testing1", {value: twoCoffees})
+
+        await CoffeeContract.connect(deployer).withdraw()
+
+        // const endingBalance = BigInt(await deployer.getBalance())
+        console.log(await deployer.getBalance())
+        
+
+    })
+
+
 })
